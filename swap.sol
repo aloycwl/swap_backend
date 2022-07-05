3349ff88666d721035f0d3a518e8181c485659eb
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2022-07-05 14:39:49 +0800

    Update swap.sol

--- swap.sol
+++ swap.sol
@@ -1,41 +1,42 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 interface IERC20{function transferFrom(address,address,uint)external;}
-contract Swap{
-    uint public fee=9999;
+contract Swap_93N{
+    uint public fee=9999; //Divide by 10000 to return 99.9%
     address private _owner;
-    mapping(address=>mapping(address=>uint[2]))public pairs;
+    mapping(address=>mapping(address=>uint))public pairs;
     constructor(){
         _owner=msg.sender;
     }
     modifier OnlyOwner(){
         require(_owner==msg.sender);_;
     }
-    function addLiqudity(address conAddr1,address conAddr2,uint amount1,uint amount2)external payable OnlyOwner{unchecked{
-        IERC20(conAddr1).transferFrom(msg.sender,address(this),amount1);
-        IERC20(conAddr2).transferFrom(msg.sender,address(this),amount2);
-        (pairs[conAddr1][conAddr2][0]+=amount1,pairs[conAddr1][conAddr2][1]+=amount2,
-        pairs[conAddr2][conAddr1][0]+=amount2,pairs[conAddr2][conAddr1][1]+=amount1);
-    }}
-    function RemoveLiqudity(address conAddr1,address conAddr2,uint amount1,uint amount2)external payable OnlyOwner{unchecked{
-        require(_owner==msg.sender);
-        IERC20(conAddr1).transferFrom(address(this),msg.sender,amount1);
-        IERC20(conAddr2).transferFrom(address(this),msg.sender,amount2);
-        (pairs[conAddr1][conAddr2][0]-=amount1,pairs[conAddr1][conAddr2][1]-=amount2,
-        pairs[conAddr2][conAddr1][0]-=amount2,pairs[conAddr2][conAddr1][1]-=amount1);
-    }}
     function setFee(uint percent)external OnlyOwner{
         fee=percent;
     }
-    function exchange(address conAddr1,address conAddr2,uint amount1)external payable{unchecked{
-        uint amount2=getPrice(conAddr1,conAddr2,amount1);
-        require(amount2>0);
-        require(amount2<=pairs[conAddr1][conAddr2][1]);
-        IERC20(conAddr1).transferFrom(msg.sender,address(this),amount1);
-        IERC20(conAddr2).transferFrom(address(this),msg.sender,amount2);
-        (pairs[conAddr1][conAddr2][0]+=amount1,pairs[conAddr1][conAddr2][1]-=amount2,
-        pairs[conAddr2][conAddr1][0]-=amount2,pairs[conAddr2][conAddr1][1]+=amount1);
+    function AddLiqudity(address addr0,address addr1,uint amt0,uint amt1)external OnlyOwner{unchecked{
+        IERC20(addr0).transferFrom(msg.sender,address(this),amt0);
+        IERC20(addr1).transferFrom(msg.sender,address(this),amt1);
+        (pairs[addr0][addr1]+=amt0,pairs[addr1][addr0]+=amt1);
     }}
-    function getPrice(address conAddr1,address conAddr2,uint amount)public view returns(uint){{
-        return pairs[conAddr1][conAddr2][1]*amount/pairs[conAddr1][conAddr2][0]*fee/10000;
+    function RemoveLiqudity(address addr0,address addr1,uint amt0,uint amt1)external OnlyOwner{unchecked{
+        IERC20(addr0).transferFrom(address(this),msg.sender,amt0);
+        IERC20(addr1).transferFrom(address(this),msg.sender,amt1);
+        (pairs[addr0][addr1]-=amt0,pairs[addr1][addr0]-=amt1);
+    }} 
+    function exchange(uint amt,address addr0,address addr1)external{unchecked{
+        uint amt2=getAmountsOut(amt,addr0,addr1);
+        require(amt2>0);
+        require(amt2<=pairs[addr1][addr0]);
+        IERC20(addr0).transferFrom(msg.sender,address(this),amt);
+        IERC20(addr1).transferFrom(address(this),msg.sender,amt2);
+        (pairs[addr0][addr1]+=amt,pairs[addr1][addr0]-=amt2);
+    }}
+    function getAmountsOut(uint amt,address addr0,address addr1)public view returns(uint){unchecked{
+        uint _D=1e18;
+        (uint d,uint _L1,uint _L2)=(amt%_D,pairs[addr0][addr1],pairs[addr1][addr0]);
+        require(amt<=_L1);
+        (amt-=d,amt/=_D); 
+        for(uint i=0;i<amt;i++)(_L2-=_L2*_D/_L1,_L1+=_D);
+        return(pairs[addr1][addr0]-_L2+(d>0?pairs[addr1][addr0]*d/pairs[addr0][addr1]:0))*fee/1e4;
     }}
 }
